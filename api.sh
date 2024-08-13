26fa92ead6f326418478823845d655ee40992656
Markus Korn <markus.korn@gmail.com>
2024-08-13 11:30:37 +0200

    Add api.sh for enhanced directory handling
    - Introduce `api.sh` script to enhance directory management and automate slumber commands
    - Improve user interaction through streamlined command execution based on input

---
+++ api.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+BASEDIR=~/devel/github.com/thekorn/api-collection
+
+if [[ $# -eq 1 ]]; then
+    selected=$1
+else
+    selected=$(printf '%s\n' "$(fd -p -t d -d 3 --min-depth 2 $BASEDIR)" | fzf)
+fi
+
+if [[ -z $selected ]]; then
+    echo "No directory selected"
+    exit 0
+fi
+
+selected=$(echo $selected | sed "s;~;$HOME;")
+
+if [ ! -d "$selected" ]; then
+  echo "$selected does not exist."
+  exit 1
+fi
+
+if [ ! -f "$selected/slumber.yml" ]; then
+  echo "$selected is not a slumber directory."
+  exit 1
+fi
+
+echo "Selected: $selected"
+
+if [ -f "$selected/.env" ]; then
+  op run --env-file="$selected/.env" -- slumber --file "$selected/slumber.yml"
+else
+  slumber --file "$selected/slumber.yml"
+fi
